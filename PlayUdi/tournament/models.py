e31b03bd60cfb1121534aa136672ee75a92b1b4d
Naif Albariqi <naif.albarqi201@hotmail.com>
2023-09-08 17:13:58 +0300

    profile account and update account

--- PlayUdi/tournament/models.py
+++ PlayUdi/tournament/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(default="2000-10-10")
+    avatar = models.ImageField(upload_to="images/", default="images/default.png")
+    # rank
+    # Trophy
